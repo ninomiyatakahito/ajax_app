cac958b158394f1ea2e47354e2a48eeb26477fc1
ninomiyatakahito <ghexcffb@gmail.com>
2020-10-01 16:30:12 +0900

    Ajax Appの準備

--- db/migrate/20200912124017_create_posts.rb
+++ db/migrate/20200912124017_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t| #実際にpostテーブルを作成する。という仕様が書かれている
       t.text :content
+      t.boolean :checked
       t.timestamps
     end
   end
